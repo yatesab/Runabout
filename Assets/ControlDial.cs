e8fa983272949c9a6e41cd44149bbee68b75c0a1
Andrew Yates <yatesab@gmail.com>
2024-02-01 14:50:27 -0700

    Small dial changes

--- Assets/ControlDial.cs
+++ Assets/ControlDial.cs
@@ -7,23 +7,99 @@ public class ControlDial : MonoBehaviour
 {
     public MeshMirror controlDialMirror;
 
-    // Start is called before the first frame update
+    [SerializeField] private int snapRotationAmount = 90;
+    [SerializeField] private float angleTolerance;
+
+    private IXRSelectInteractor interactor;
+    private float startAngle;
+    private bool requiresStartAngle = true;
+    private bool shouldGetHandRotation = false;
+    private XRGrabInteractable _grabInteractable;
+    
     void Start()
     {
-        // Get XRGrabInteractable for isGrabbing check
-        XRGrabInteractable _grabInteractable = GetComponentInChildren<XRGrabInteractable>();
+        _grabInteractable = GetComponent<XRGrabInteractable>();
 
         _grabInteractable.hoverEntered.AddListener(HandleHoverEnter);
         _grabInteractable.hoverExited.AddListener(HandleHoverExit);
+        _grabInteractable.selectEntered.AddListener(HandleGrab);
+        _grabInteractable.selectExited.AddListener(HandleLetgo);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(shouldGetHandRotation)
+        {
+            var rotationAngle = GetInteractorRotation();
+            
+            GetRotationDistance(rotationAngle);
+        }
+    }
+
+    public float GetInteractorRotation() => interactor.transform.eulerAngles.z;
+
+    private void GetRotationDistance(float currentAngle)
+    {
+        if (requiresStartAngle)
+        {
+            startAngle = currentAngle;
+            requiresStartAngle = false;
+        }
+
+        var angleDifference = Mathf.Abs(startAngle - currentAngle);
+
+        if (angleDifference > angleTolerance)
+        {
+            if(angleDifference > 270f)
+            {
+                float angleCheck = CheckAngle(currentAngle, startAngle);
+                
+                if (angleCheck < angleTolerance) return;
+                else if (startAngle < currentAngle)
+                {
+                    RotateDialCounterClockwise();
+                }
+                else if(startAngle > currentAngle) 
+                {
+                    RotateDialClockwise();
+                }
+
+                startAngle = currentAngle;
+            } else
+            {
+                if (startAngle < currentAngle)
+                {
+                    RotateDialClockwise();
+                } else if (startAngle > currentAngle)
+                {
+                    RotateDialCounterClockwise();
+                }
+                    
+                startAngle = currentAngle;
+            }
+        }
+    }
+
+    private float CheckAngle(float currentAngle, float startAngle) => (360f - currentAngle) + startAngle;
+
+    private void RotateDialClockwise()
+    {
+        // Rotate Dial Clockwise and the mirror
+        transform.Rotate(0f, 0f, snapRotationAmount);
+
         controlDialMirror.MirrorRotation(transform.localRotation);
     }
 
-    public void HandleHoverEnter(HoverEnterEventArgs args)
+    private void RotateDialCounterClockwise()
+    {
+        // Rotate dial counter clockwise and the mirror
+        transform.Rotate(0f, 0f, -snapRotationAmount);
+
+        controlDialMirror.transform.localEulerAngles = transform.localEulerAngles;
+    }
+
+    private void HandleHoverEnter(HoverEnterEventArgs args)
     {
         // Do hover things here
         if (controlDialMirror.hasHover)
@@ -32,12 +108,27 @@ public class ControlDial : MonoBehaviour
         }
     }
 
-    public void HandleHoverExit(HoverExitEventArgs args)
+    private void HandleHoverExit(HoverExitEventArgs args)
     {
         // Do hover things here
         if (controlDialMirror.hasHover)
         {
             controlDialMirror.DeactivateHover();
         }
+    }
+
+    private void HandleGrab(SelectEnterEventArgs args)
+    {
+        interactor = _grabInteractable.interactorsSelecting[0];
+
+        shouldGetHandRotation = true;
+        startAngle = 0f;
+    }
+
+
+    private void HandleLetgo(SelectExitEventArgs args)
+    {
+        shouldGetHandRotation = false;
+        requiresStartAngle = true;
     }
 }
